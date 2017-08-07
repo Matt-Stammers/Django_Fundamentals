3e8d4f69d963400a884a473e9652a49f9351b6bf
Matt Stammers <matthewstammers@googlemail.com>
2017-08-07 23:50:34 +0100

    Add files via upload

---
+++ first_project/first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
+
+# Create your views here.
+
+def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'accessrecords': webpages_list}
+    return render(request, 'first_app/index.html', context = date_dict)
+
+
+# first_dict = {'insert_me': "Hello there Clinical Developer!"}
